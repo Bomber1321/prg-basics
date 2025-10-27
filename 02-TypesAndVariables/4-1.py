c8aef99b7be057e873d2286ab7b86a893c09994a
Norbert Czerw <norbertczerw1321@gmail.com>
2025-10-27 02:12:57 +0100

    new files

---
+++ 02-TypesAndVariables/4-1.py
@@ -0,0 +1,9 @@
+###
+# Printing student's personal data
+#
+height = 180
+age = 20
+name = "Adam"
+print(f"My name is {name}.")
+print(f"I am {age} years old, and my height is {height} cm.")
+print(f"In 6 years I will be {age+6}")
